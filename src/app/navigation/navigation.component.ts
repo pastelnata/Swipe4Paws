77ad413fe12683beaf060870dd7c52b1e09b91f0
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-09-21 18:57:30 +0200

    Initial file structure

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      navigation works!
+    </p>
+  `,
+  styleUrl: './navigation.component.css'
+})
+export class NavigationComponent {
+
+}
---
+++ src/app/pets-listing/pets-listing.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-listing',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      pets-listing works!
+    </p>
+  `,
+  styleUrl: './pets-listing.component.css'
+})
+export class PetsListingComponent {
+
+}
