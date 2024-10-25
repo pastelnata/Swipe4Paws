22767da39cff1d20d53e9612b28cf2065c644543
sduhubert <husyl23@student.sdu.dk>
2024-10-25 16:01:12 +0200

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home-view/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,NavigationComponent],
+  imports: [RouterOutlet, HomeComponent, NavigationComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
