0786c097b979d13709386486ef9805ae2c56e6c4
sduhubert <husyl23@student.sdu.dk>
2024-11-08 11:55:16 +0100

    Add profile bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home-view/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from "./header/header.component";
 import { HomeModule } from './home/home.module';
+import { ProfileBarModule } from "./profile-bar/profile-bar.module";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavigationComponent, HeaderComponent, HomeModule],
+  imports: [
+    RouterOutlet, 
+    CommonModule,
+    HomeComponent, 
+    NavigationComponent, 
+    HeaderComponent, 
+    HomeModule, 
+    ProfileBarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'swipe4paws';
+
+  isProfileBarVisible: boolean = false;
+
+  toggleProfileBar() {
+    this.isProfileBarVisible = !this.isProfileBarVisible;
+  }
 }
