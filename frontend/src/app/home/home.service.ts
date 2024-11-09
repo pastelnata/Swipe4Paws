d2dbd085e316dc67a014ec915a4f02ab6500dcbe
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-11-09 14:30:11 +0100

    Other filters fix

--- frontend/src/app/home/home.service.ts
+++ frontend/src/app/home/home.service.ts
@@ -9,7 +9,7 @@ export class HomeService {
   private petsListingList: PetsListing[] = [
     { id: 1, name: "a", postDate: new Date('2024-10-20'), gender: "Female", age: 2, type: "cat", behavior: ["friendly", "calm"], city: "Odense", photo: "/assets/kitty1.jpg" },
     { id: 2, name: "c", postDate: new Date('2024-10-19'), gender: "Male", age: 3, type: "dog", behavior: ["good with children", "lazy"], city: "Sonderborg", photo: "/assets/kitty1.jpg" },
-    { id: 3, name: "b", postDate: new Date('2024-10-22'),gender: "Female", age: 6, type: "dog", behavior: ["friendly", "playful"] ,city: "Sonderborg", photo: "/assets/kitty1.jpg" },
+    { id: 3, name: "b", postDate: new Date('2024-10-22'),gender: "Female", age: 6, type: "kaczka", behavior: ["friendly", "playful"] ,city: "Sonderborg", photo: "/assets/kitty1.jpg" },
     { id: 4, name: "d", postDate: new Date('2024-10-21'),gender: "Male", age: 1, type: "cat", behavior: ["aggressive", "loud"],city: "Sonderborg", photo: "/assets/kitty1.jpg" }
   ];
 
@@ -45,13 +45,47 @@ export class HomeService {
     this.applyFilters();
   }
 
-  applyFilters() {
+  /**
+   Apply filters to the pets list
+   If no filters are selected, the function returns the original list
+   */
+   applyFilters() {
     this.filteredPetsList = this.petsListingList.filter(pet => {
-      const matchesName = pet.name.toLowerCase().includes(this.nameFilter.toLowerCase());
-      const matchesType = this.typeFilter ? pet.type.toLowerCase() === this.typeFilter.toLowerCase() : true;
-      const matchesGender = this.genderFilter ? pet.gender.toLowerCase() === this.genderFilter.toLowerCase() : true;  
-      const matchesbehavior = this.currentFilters.length === 0 || this.currentFilters.every(filter => pet.behavior.includes(filter));
-      return matchesName && matchesType && matchesGender && matchesbehavior;
+      // Check if the pet matches the name filter
+      const petMatchesName = this.nameFilter
+        ? pet.name.toLowerCase().includes(this.nameFilter.toLowerCase())
+        : true;
+
+      // Check if the pet matches the type filter
+      let petMatchesType = false;``
+
+        if(this.typeFilter === "other") {
+          if(!(pet.type.toLowerCase() === "cat" || pet.type.toLowerCase() === "dog")){
+            petMatchesType = true;
+          }
+        }else {
+          petMatchesType = this.typeFilter
+          ? pet.type.toLowerCase() === this.typeFilter.toLowerCase()
+          : true;
+        }
+
+      // Check if the pet matches the gender filter
+      const petMatchesGender = this.genderFilter
+        ? pet.gender.toLowerCase() === this.genderFilter.toLowerCase()
+        : true;
+
+      // Check if the pet matches any of the behavior filters
+      const petMatchesBehavior = this.currentFilters.length === 0
+        ? true
+        : this.currentFilters.every(filter => pet.behavior.includes(filter));
+
+      // Return true if the pet matches all the filters
+      return (
+        petMatchesName &&
+        petMatchesType &&
+        petMatchesGender &&
+        petMatchesBehavior
+      );
     });
   }
 
