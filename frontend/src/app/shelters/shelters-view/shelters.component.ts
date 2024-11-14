fe3352a28f52fe68261703487875eee4595b54ba
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-11-14 21:43:37 +0100

    pets are displayed on the main page

--- frontend/src/app/shelters/shelters-view/shelters.component.ts
+++ frontend/src/app/shelters/shelters-view/shelters.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PetDataService } from '../../pet-data/pet-data.service';
 import { PetsListing } from '../../models/pets-listing';
-
+// import { HomeService } from '../../home/home.service';
 @Component({
   selector: 'app-shelters',
   standalone: true,
@@ -10,23 +10,23 @@ import { PetsListing } from '../../models/pets-listing';
   styleUrl: './shelters.component.css'
 })
 export class SheltersComponent {
-  pet: PetsListing = {
-    id: 0,
-    name: '',
-    age: 0,
-    type: '',
-    gender: '',
-    behavior: [],
-    city: '',
-    postDate: new Date(),
-    photo: ''
-  };
+  // pet: PetsListing = {
+  //   id: 0,
+  //   name: '',
+  //   age: 0,
+  //   type: '',
+  //   gender: '',
+  //   behavior: [],
+  //   city: '',
+  //   postDate: new Date(),
+  //   photo: ''
+  // };
 
   constructor(private petDataService: PetDataService) {}
 
-  onSubmit(): void {
-    this.pet.id = Date.now();
-    this.petDataService.addPet(this.pet);
-    this.pet = { ...this.pet, id: 0, name: '', age: 0, type: '', gender: '', behavior: [], city: '', photo: ''};
-  }
+  // onSubmit(): void {
+  //   this.pet.id = Date.now();
+  //   this.petDataService.addPet(this.pet);
+  //   this.pet = { ...this.pet, id: 0, name: '', age: 0, type: '', gender: '', behavior: [], city: '', photo: ''};
+  // }
 }
