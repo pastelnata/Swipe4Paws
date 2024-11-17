0fb7bdd01512290d8085be860f2a57e3eaf136f3
sduhubert <husyl23@student.sdu.dk>
2024-11-17 18:06:11 +0100

    Fix routing, paths

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import routes from '../routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import routes from '../routes';
