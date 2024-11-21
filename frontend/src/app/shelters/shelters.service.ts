c2334d3284aedccb6453ffd9fbd6a735e0dbc36f
Jakub Smilowski <jakub.smilowski@protonmail.com>
2024-11-21 17:53:54 +0100

    basic page structure + conection to backend

--- frontend/src/app/shelters/shelters.service.ts
+++ frontend/src/app/shelters/shelters.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SheltersListing } from '../models/shelters-listing';
 @Injectable({
   providedIn: 'root'
 })
 export class SheltersService {
 
-  constructor() { }
+  sheltersList: SheltersListing[] = [];
+
+  constructor(private http: HttpClient) {
+    this.getAllShelters();
+   }
+
+  getAllShelters(): Observable<SheltersListing[]> {
+    return this.http.get<SheltersListing[]>('http://localhost:3000/shelters');
+  }
+
+  
 }
