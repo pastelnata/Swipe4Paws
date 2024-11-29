2b4a8d19c65e369edc8f0fb5fc662aebefbaceb1
Ali Krist <alikr23@student.sdu.dk>
2024-11-29 22:59:35 +0100

    login page html type script css

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -7,5 +7,6 @@ const userController = new UserController();
 // Defines the routes
 userRoutes.get('/', (req, res) => userController.getAllUsers(req, res));
 userRoutes.post('/register', (req, res) => userController.createUser(req, res));
+userRoutes.post('/user', (req, res) => userController.loginUser(req,res));
 
 export default userRoutes;
--- frontend/src/app/profile-bar/profile-bar-view/profile-bar-view/profile-bar-view.component.ts
+++ frontend/src/app/profile-bar/profile-bar-view/profile-bar-view/profile-bar-view.component.ts
@@ -1,9 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile-bar-view',
   templateUrl: './profile-bar-view.component.html',
-  styleUrl: './profile-bar-view.component.css'
+  styleUrl: './profile-bar-view.component.css',
+  standalone: true,
+  imports: [MatIconModule]
 })
 export class ProfileBarViewComponent {
   @Output() close = new EventEmitter<void>();
