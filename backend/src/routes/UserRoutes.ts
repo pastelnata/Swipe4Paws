42ad701569141d94b9759e349319e4ffcf6486b2
alikrist <144455345+alikrist@users.noreply.github.com>
2024-12-01 22:57:02 +0100

    Update UserRoutes.ts in backend

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -9,6 +9,5 @@ userRoutes.get('/', (req, res) => userController.getAllUsers(req, res));
 //userRoutes.get('/:id', (req, res) => userController.getUserById(req, res));
 userRoutes.post('/register', (req, res) => userController.createUser(req, res));
 userRoutes.post('/login', (req, res) => userController.loginUser(req,res));
-//userRoutes.post('/logout', (req, res) => userController.logoutUser(req,res));
 
 export default userRoutes;
