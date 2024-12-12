b67c100036722e60763598ddae242ea0a4aaf4ba
Kuba <kubuniek11@gmail.com>
2024-12-12 19:10:43 +0100

    update behavior in behavior controller working now

--- backend/src/controllers/BehaviorController.ts
+++ backend/src/controllers/BehaviorController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import PetBehavior from "../models/PetBehaviorModel";
 
 class BehaviorController {
+  // Get all behaviors
     public async getAllBehaviors(req: Request, res: Response) {
       try {
         const behaviors = await PetBehavior.findAll();
@@ -11,12 +12,27 @@ class BehaviorController {
       }
     }
 
+    //add behavior used in petcontroller when adding pets.
     public async addBehavior(req: Request, res: Response) {
       try {
         const behavior = await PetBehavior.create(req.body);
         res.json(behavior);
       } catch (error) {
         console.error("Error adding behavior:", error);
+      }
+    }
+
+    //Update call
+    public async updateBehavior(req: Request, res: Response) {
+      try {
+        const behavior = await PetBehavior.findByPk(req.params.id);
+        if (!behavior) {
+          return res.status(404).json({ error: "Behavior not found!" });
+        }
+        await behavior.update(req.body);
+        res.json(behavior);
+      } catch (error) {
+        console.error("Error updating behavior:", error);
       }
     }
 
--- backend/src/routes/BehaviorRoutes.ts
+++ backend/src/routes/BehaviorRoutes.ts
@@ -7,5 +7,6 @@ const behaviorController = new BehaviorController();
 // Defines the routes
 behaviorRoutes.get('/', (req, res) => behaviorController.getAllBehaviors(req, res));
 behaviorRoutes.post('/add', (req, res) => behaviorController.addBehavior(req, res));
+behaviorRoutes.put('/update/:id', (req, res) => behaviorController.updateBehavior(req, res));
 
 export default behaviorRoutes;
