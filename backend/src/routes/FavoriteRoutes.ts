fc24303202bd8c9b62f56df3b724d4b86553c1fa
sduhubert <husyl23@student.sdu.dk>
2024-12-12 23:33:32 +0100

    Assign favourites to userid in Home, Swipe, Favourites

--- backend/src/routes/FavoriteRoutes.ts
+++ backend/src/routes/FavoriteRoutes.ts
@@ -8,6 +8,12 @@ const favoriteController = new FavoriteController();
 // Defines the routes
 favoriteRoutes.post('/add', isUser, (req, res) => favoriteController.addFavorite(req, res));
 favoriteRoutes.delete('/delete', isUser, (req, res) => favoriteController.deleteFavorite(req, res));
-favoriteRoutes.get('/get', isUser, (req, res) => favoriteController.getAllFavorites(req, res));
+favoriteRoutes.get('/get', isUser, (req, res) => {
+    const requestedUserId: string | null = <string>req.query.userId || null;
+    let userId: number = 0;
+    if(requestedUserId) userId=parseInt(requestedUserId); 
+
+    favoriteController.getAllFavorites(userId, req, res);
+});
 
 export default favoriteRoutes;
